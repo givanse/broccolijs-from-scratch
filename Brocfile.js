3424b48410da4fead2da80a3009ab81773bf5581
Gastón I. Silva E <silvag777@gmail.com>
2015-04-19 17:53:02 -0500

    add style.css

--- Brocfile.js
+++ Brocfile.js
@@ -1,8 +1,15 @@
 var funnel = require('broccoli-funnel');
+var concat = require('broccoli-concat');
+var mergeTrees = require('broccoli-merge-trees');
 
 var html = funnel('.', {
   files   : ['index.html'],
   destDir : '/'
 });
 
-module.exports = html;
+var styles = concat('.', {
+               inputFiles : ['style.css'],
+               outputFile : '/site.css'
+             });
+
+module.exports = mergeTrees([html, styles]);
